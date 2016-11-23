fd748a52996fb279bbf3ddbd542c6c125bec9d10
bianyichao <370355340@qq.com>
2016-11-23 09:38:31 +0800

    边弈超

--- xishuashua/Application/Runtime/Cache/Home/6155fa2208c58db58ca7f502e78fee41.php
+++ xishuashua/Application/Runtime/Cache/Home/6155fa2208c58db58ca7f502e78fee41.php
@@ -3,11 +3,11 @@
 <head>
 <meta charset="utf-8">
 <title>喜刷刷家装后台管理中心</title>
-<link rel="stylesheet" href="/xishuashua/Public/home/css/bootstrap.css">
-<link href="/xishuashua/Public/home/css/jquery-ui.css" rel="stylesheet">
-<link href="/xishuashua/Public/home/css/mystyle.css" rel="stylesheet">
-<script src="/xishuashua/Public/home/js/jquery.js"></script>    
-<script src="/xishuashua/Public/home/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="/h5_2014_xishuashua/xishuashua/Public/home/css/bootstrap.css">
+<link href="/h5_2014_xishuashua/xishuashua/Public/home/css/jquery-ui.css" rel="stylesheet">
+<link href="/h5_2014_xishuashua/xishuashua/Public/home/css/mystyle.css" rel="stylesheet">
+<script src="/h5_2014_xishuashua/xishuashua/Public/home/js/jquery.js"></script>    
+<script src="/h5_2014_xishuashua/xishuashua/Public/home/js/bootstrap.min.js"></script>
 <style type="text/css">
   .sidebar{
     background-color: rgb(35,100,0);
@@ -28,7 +28,7 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <!-- Brand and toggle get grouped for better mobile display -->
   <div class="navbar-header">
-     <img src="/xishuashua/Public/home/images/logo.png" >
+     <img src="/h5_2014_xishuashua/xishuashua/Public/home/images/logo.png" >
     <span class="navbar-logo" > 喜刷刷家装后台管理中心</span>
    </div>
 
@@ -48,20 +48,20 @@
         <div id="accordion">
           <h3><a href="#"><span class="glyphicon glyphicon-lock"></span>管理员账户管理<span class="glyphicon glyphicon-chevron-down"></span></a></h3>
            <ul>
-              <li ><a href="/xishuashua/index.php/Home/admin/user_list">管理员信息管理</a></li>
-              <li><a href="/xishuashua/index.php/Home/admin/publish_news">发布新闻管理</a></li>
-              <li><a href="/xishuashua/index.php/Home/admin/manage_news">管理新闻管理</a></li>                
-              <li ><a href="/xishuashua/index.php/Home/admin/add">添加管理员</a></li>
-              <li ><a href="/xishuashua/index.php/Home/admin/user_update">修改管理员信息</a></li>
-              <li ><a href="/xishuashua/index.php/Home/admin/password_update">修改管理员密码</a></li>
-              <li ><a href="/xishuashua/index.php/Home/admin/update_news">修改新闻信息</a></li>
+              <li ><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/admin/user_list">管理员信息管理</a></li>
+              <li><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/admin/publish_news">发布新闻管理</a></li>
+              <li><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/admin/manage_news">管理新闻管理</a></li>                
+              <li ><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/admin/add">添加管理员</a></li>
+              <li ><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/admin/user_update">修改管理员信息</a></li>
+              <li ><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/admin/password_update">修改管理员密码</a></li>
+              <li ><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/admin/update_news">修改新闻信息</a></li>
           </ul>
             <h3><a href="#"><span class="glyphicon glyphicon-file"></span>个人账户管理<span class="glyphicon glyphicon-chevron-down"></span></a></h3>
             <ul>
-              <li ><a href="/xishuashua/index.php/Home/admin/user_list">个人用户信息管理</a></li>
-              <li><a href="/xishuashua/index.php/Home/admin/put_list">个人招标管理</a></li>
-              <li><a href="/xishuashua/index.php/Home/admin/news">个人文章管理</a></li>
-              <li ><a href="/xishuashua/index.php/Home/admin/user_update">修改个人信息</a></li>
+              <li ><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/personal/user_list">个人用户信息管理</a></li>
+              <li><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/personal/put_list">个人招标管理</a></li>
+              <li><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/personal/news">个人文章管理</a></li>
+              <li ><a href="/h5_2014_xishuashua/xishuashua/index.php/Home/personal/user_update">修改个人信息</a></li>
           </ul>
             <h3><a href="#"><span class="glyphicon glyphicon-book"></span>企业账户管理<span class="glyphicon glyphicon-chevron-down"></span></a></h3>
             <ul>
@@ -91,14 +91,14 @@
     	    <div class="list-group ">
     	      <div class="list-group-item">
     	        <form role="form" >
-    	          <div class="input-group"> <span class="input-group-addon">用<img src="/xishuashua/Public/home/images/em.png" alt="" width="6" height="20">户<img src="/xishuashua/Public/home/images/em.png" alt="" width="6" height="20">名：</span>
+    	          <div class="input-group"> <span class="input-group-addon">用<img src="/h5_2014_xishuashua/xishuashua/Public/home/images/em.png" alt="" width="6" height="20">户<img src="/h5_2014_xishuashua/xishuashua/Public/home/images/em.png" alt="" width="6" height="20">名：</span>
     	            <input type="text" class="form-control" placeholder="administrator">
   	            </div>
     	          <div class="input-group"> <span class="input-group-addon">电子邮箱：</span>
     	            <input type="text" class="form-control" placeholder="86823499@qq.com">
   	            </div>
     	          <div class="input-group">
-    	            <button type="submit" class="btn btn-primary "> &nbsp;&nbsp;保<img src="/xishuashua/Public/home/images/em.png" alt="" width="20" height="20">存&nbsp;&nbsp;</button>
+    	            <button type="submit" class="btn btn-primary "> &nbsp;&nbsp;保<img src="/h5_2014_xishuashua/xishuashua/Public/home/images/em.png" alt="" width="20" height="20">存&nbsp;&nbsp;</button>
   	            </div>
   	          </form>
   	        </div>
@@ -112,8 +112,9 @@
 
 
 </div>
-<script src="/xishuashua/Public/home/js/jquery-ui.js"></script>
+<script src="/h5_2014_xishuashua/xishuashua/Public/home/js/jquery-ui.js"></script>
 <script>
+
 $( "#accordion" ).accordion({
       heightStyle: "content"
     });
