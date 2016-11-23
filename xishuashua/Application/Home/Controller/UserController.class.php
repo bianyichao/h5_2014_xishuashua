3650bb5ba76e1ea1cd804c2e37d918d60579854f
曹晓丽 <1277326799@qq.com>
2016-11-23 09:03:33 +0800

    默认的

---
+++ xishuashua/Application/Home/Controller/UserController.class.php
@@ -0,0 +1,26 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class UserController extends Controller {
+    public function login(){
+    	layout(false);
+       if (IS_POST) {
+     		$adminUser=M("admin");
+     		$condition=array(
+     			"admin"=>I("post.username"),
+     		    "passsword"=>I("post.pwd")
+     		    );
+     		$result=$adminUser->where($condition)->count();
+     		if ($result) {
+     			session("admin",I("post.username"));
+                $this->success("登录成功",U("Index/main"));
+     		}
+     		else{
+     			$this->error("用户名或密码错误！");
+     		}
+     	}
+     	else{
+           $this->display();
+     	}
+   }
+}
