d0c9ee45240984035a5713b814cf6c7009058786
bianyichao <370355340@qq.com>
2016-12-08 14:36:20 +0800

    前台搭建啦啦啦

--- xishuashua/Application/Admin/Controller/CompanyController.class.php
+++ xishuashua/Application/Admin/Controller/CompanyController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class CompanyController extends Controller {
     public function index(){
         $this->display();
     }
