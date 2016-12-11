5ed9d69604ca0254530d7790fb80d7fdfd156287
qiuyang951107 <912067571@qq.com>
2016-12-11 19:15:31 +0800

    喜刷刷v2.1

--- 喜刷刷前台/js/text.js
+++ 喜刷刷前台/js/text.js
@@ -15,20 +15,4 @@ $(function(){
 	  		if(num4.length>25){
 	  			$("#d").html(num4.substr(0,25)+"......");
 	  		}
-			var num5=$("#e").html();
-			if(num5.length>17){
-				$("#e").html(num5.substr(0,17)+"......");
-			}
-			var num6=$("#f").html();
-			if(num6.length>17){
-				$("#f").html(num6.substr(0,17)+"......");
-			}
-			var num7=$("#g").html();
-			if(num7.length>17){
-				$("#g").html(num7.substr(0,17)+"......");
-			}
-			var num8=$("#h").html();
-			if(num8.length>17){
-				$("#h").html(num8.substr(0,17)+"......");
-			}
 	  	});
